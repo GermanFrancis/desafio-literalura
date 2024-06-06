930c598e74ee02e7986b82ce7f4fbc47607dedca
German <German_francis@hotmail.com>
2024-06-06 12:41:43 -0500

    Add: data project

---
+++ src/main/java/com/aluracursos/literalura_challenge/LiteraluraChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.literalura_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/literalura_challenge/LiteraluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.literalura_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
